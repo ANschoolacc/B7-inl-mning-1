52a27ef7bfbd0b12fd5fab6a4651c741cdd2d46c
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-03-19 17:10:12 +0100

    Made logic and tests for uniqueletters parameter

--- randomWord.js
+++ randomWord.js
@@ -1,6 +1,17 @@
-
-export default function randomWord(wordList, wordLength, uniqueLetters){
+export default function randomWord(wordList, wordLength, uniqueLetters) {
   const validList = wordList.filter((x) => x.length == wordLength);
+
+  for (let i = 0; i < validList.length; i++) {
+    const letters = validList[i].split("");
+    let letterCount = {};
+
+    letters.forEach((letter) => {letterCount[letter] = (letterCount[letter] || 0) + 1;});
+    
+    if (Object.values(letterCount).some((x) => x > 1) && uniqueLetters === true) {
+      validList.splice(i, 1);
+    }
+  }
+
   const word = validList[Math.floor(Math.random() * validList.length)];
-  return word
-};
+  return word;
+}
--- randomWord.spec.js
+++ randomWord.spec.js
@@ -1,4 +1,4 @@
-import {describe, expect, it} from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 
 import randomWord from './randomWord';
 
@@ -13,30 +13,53 @@ Output: Det slumpmässigt utvalda ordet
 
 1.Få tillbaks en sträng
 2.Skicka in en lista med ord och en siffra för hur många bokstäver ordet får vara. Få tillbaka ett ord med rätt längd ur listan.
-3.
+3.Skicka in en lista med ord och en siffra för hur många bokstäver ordet får vara samt en indikator ifall alla bostäver måste vara unika. Om indikation för unika bostäver är true: Få tillbaka ett ord ur listan med rätt längd innehållande endast unika bokstäver .
 4.
 */
-describe('randomWord()',() => {
+describe('randomWord()', () => {
   it('Always return type of string as output', () => {
     const output = randomWord(list, 3, true);
     expect(typeof output).toBe('string');
   });
 
   it('Return random word from list with three letters', () => {
-    const output = randomWord(list, 3, true);
-    if(output != 'HUS' && output !='NOD'){
-      console.log(output)
-    expect(output).toBe('BAS');
-  } else if(output != 'BAS' && output != 'HUS'){
-    expect(output).toBe('NOD');
-  }else if(output != 'BAS' && output != 'NOD'){
-    expect(output).toBe('HUS');
-  }
+    for (let i = 0; i < 4; i++) {
+      const output = randomWord(list, 3, true);
+      if (output != 'HUS' && output != 'NOD') {
+        expect(output).toBe('BAS');
+      } else if (output != 'BAS' && output != 'HUS') {
+        expect(output).toBe('NOD');
+      } else if (output != 'BAS' && output != 'NOD') {
+        expect(output).toBe('HUS');
+      }
+    }
   });
+
+  it('Test that random word can contain more than one instance of same letter', () => {
+    for (let i = 0; i < 4; i++) {
+      const output = randomWord(list, 5, false);
+      if (output != 'AGENT' && output != 'CYKEL') {
+        expect(output).toBe('HALLÅ');
+      } else if (output != 'AGENT' && output != 'HALLÅ') {
+        expect(output).toBe('CYKEL');
+      } else if (output != 'CYKEL' && output != 'HALLÅ') {
+        expect(output).toBe('AGENT');
+      }
+    }
+  });
+
+  it('Test that random word can only contain one instance of same letter', () => {
+    for (let i = 0; i < 4; i++) {
+      const output = randomWord(list, 5, true);
+      console.log(output)
+      if (output != 'CYKEL') {
+        expect(output).toBe('AGENT');
+      } else if (output != 'AGENT') {
+        expect(output).toBe('CYKEL');
+      }
+    }
+  });
+
 })
 
-const list = ['BABBEL', 'CYKEL', 'HUS', 'HALLÅ', 'HUND', 'KATT', 'VARULV','BAS' ,'TE', 'KORRIDOR', 'ABBORRE','ADVOKAT', 'NOD'];
-
-async function expectOr(){
-
-}
+const list = ['BABBEL', 'CYKEL', 'HUS', 'HALLÅ', 'HUND', 'KATT', 'VARULV', 'BAS', 'TE', 'KORRIDOR', 'ABBORRE', 'ADVOKAT', 'NOD', 'AGENT'];
